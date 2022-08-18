8f999e4696edc4b48c3f1bb6c9e2046d6afd10b6
Max Kaemmerer <maxkaemmerer.hb@gmail.com>
2022-08-18 20:13:09 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maxkaemmerer/go-message
+
+go 1.19
---
+++ message.go
@@ -0,0 +1,47 @@
+package message
+
+import "fmt"
+
+type MessageBus interface {
+	Dispatch(msg Message) error
+}
+
+type simpleMessageBus struct {
+	handlerMap map[string][]Handler
+}
+
+func (mb *simpleMessageBus) Dispatch(msg Message) error {
+	namespace := fmt.Sprintf("%s-%s", msg.Context(), msg.Name())
+	handlers, ok := mb.handlerMap[namespace]
+	if ok {
+		for _, handler := range handlers {
+			err := handler.Handle(msg)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func NewSimpleMessageBus(handlers []Handler) MessageBus {
+	handlerMap := make(map[string][]Handler)
+	for _, handler := range handlers {
+		namespace := fmt.Sprintf("%s-%s", handler.ContextName(), handler.MessageName())
+		handlerMap[namespace] = append(handlerMap[namespace], handler)
+	}
+	return &simpleMessageBus{
+		handlerMap: handlerMap,
+	}
+}
+
+type Message interface {
+	Name() string
+	Context() string
+}
+
+type Handler interface {
+	ContextName() string
+	MessageName() string
+	Handle(msg Message) error
+}
